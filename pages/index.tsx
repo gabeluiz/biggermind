3f6312fad27a1c4c66551b1676a8227da87593cd
Gabriel Luiz <gabeluiz10@gmail.com>
2021-01-23 01:31:26 -0300

    new api auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import "tailwindcss/tailwind.css";
 import { NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 const Home: NextPage = () => {
+    const [ session, loading ] = useSession();
     return (
         <div>
-            <div className="py-10">
-                <h1 className="text-5xl text-center text-accent-1">
-                    LOUCO NÉ
-                </h1>
+            {!session && (
+            <div className="text-3xl">
+                Not signed in <br/>
+                <button onClick={():Promise<void> => signIn('auth0')}>Sign in</button>
             </div>
+            )}
+            {session && (
+            <div className="text-3xl">
+                Signed in as {session.user.email} <br/>
+                <button onClick={():Promise<void> =>  signOut()}>Sign out</button>
+            </div>
+            )}
         </div>
 
     );
