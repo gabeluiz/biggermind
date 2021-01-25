b68ac499c587c6153bd6755fcd31819908c732d0
Gabriel Luiz <gabeluiz10@gmail.com>
2021-01-25 16:44:38 -0300

    new title

--- pages/_app.js
+++ pages/_app.js
@@ -49,7 +49,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>Notus NextJS by Creative Tim</title>
+          <title>BiggerMind</title>
         </Head>
         <Layout>
           <Component {...pageProps} />
